510c1205c30de150075f8f35826598cbc8180607
samuelfsd <samuel.estudo778@gmail.com>
2024-02-20 18:44:40 -0300

    feat: initial header

--- src/components/section/Header.tsx
+++ src/components/section/Header.tsx
@@ -1,14 +1,15 @@
-import { Avatar } from "@/components/ui/avatar";
+import { Avatar, AvatarImage, AvatarFallback } from "@/components/ui/avatar";
 
 export const Header = () => {
   return (
-    <div className="container mx-auto px-4 md:px-6 lg:px-8">
+    <div className="container mx-auto px-4 md:px-6 lg:px-8 bg-cyan-6">
       <header className="flex h-20 w-full shrink-0 items-center px-4 md:px-6">
-        <div>
-          <Avatar className="flex center items-center justify-center text-white bg-slate-700 w-14 h-14">
-            SM
+        <a href="https://github.com/samuelfsd">
+          <Avatar className="flex center items-center justify-center text-white w-14 h-14">
+            <AvatarImage src="https://github.com/samuelfsd.png" />
+            <AvatarFallback>SM</AvatarFallback>
           </Avatar>
-        </div>
+        </a>
 
         <div className="ml-auto flex gap-2">
           <a
