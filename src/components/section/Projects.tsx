85d7205f70cd7b2bcdb74717370c410cf0547952
samuelfsd <samuel.estudo778@gmail.com>
2024-02-23 17:21:16 -0300

    feat: create section Projects

---
+++ src/components/section/Projects.tsx
@@ -0,0 +1,9 @@
+export const Projects = () => {
+  return (
+    <section>
+      <div className="flex items-center justify-around mt-32 mr-32 ml-32">
+        <h1>projects</h1>
+      </div>
+    </section>
+  );
+};
