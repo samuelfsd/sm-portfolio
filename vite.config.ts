1fcffa37c761d0abdb86388a533b1a38d61c68ae
samuelfsd <samuel.estudo778@gmail.com>
2024-03-05 20:46:22 -0300

    chore: add config deploy

--- vite.config.ts
+++ vite.config.ts
@@ -4,10 +4,10 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [react()],
+  base: "/deploy-vite/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  base: "/deploy-vite",
 });
