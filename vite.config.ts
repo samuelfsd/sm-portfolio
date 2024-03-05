f84cb4f8d56f37add6c775c683e2bee4320edd42
samuelfsd <samuel.estudo778@gmail.com>
2024-03-05 20:53:38 -0300

    fix: change base url

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,6 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/deploy-vite/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
